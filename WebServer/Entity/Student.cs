5adf44e9abe4bfd42d29a55ac9c63a7b53e54032
Dmitriy Vedernikov <dim01-02@mail.ru>
2022-04-03 18:21:26 +0300

    add WebServer

---
+++ WebServer/Entity/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WebServer.Entity
+{
+    public class Student
+    {
+
+        public int id { get; set; }
+        public string fio { get; set; }
+        public int age { get; set; }
+        public string email { get; set; }
+
+        public static List<Student> Students { get; } = new(new[]
+        {
+            new Student(){ id = 1, fio = "Ведерников Дмитрий Михайлович", age = 19, email = "email1@mail.ru" },
+            new Student(){ id = 2, fio = "Бессонов Иван Анатольевич", age = 20, email = "email2@mail.ru" },
+            new Student(){ id = 3, fio = "Кокорин Егор Дмитриевич", age = 19, email = "email3@mail.ru" },
+        });
+
+        public static Student? GetByID(dynamic id)
+        {
+            return Students.FirstOrDefault(stud => stud.id == id);
+        }
+
+        public static string Serialize(Student stud)
+        {
+            return JsonSerializer.Serialize(stud);
+        }
+
+    }
+}
---
+++ WebServer/Entity/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServer.Entity;
+
+public class User
+{
+    public string login { get; set; }
+    public string pass { get; set; }
+
+    public static List<User> Users = new List<User>()
+    {
+        new User() { login = "log", pass = "pass" },
+        new User() { login = "log1", pass = "pass1" },
+        new User() { login = "log2", pass = "pass2" },
+    };
+
+}
